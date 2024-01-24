6c49ef456d0f1b4ca3fee28885477f24e5e19312
zxb360 <zxb360@gmail.com>
2024-01-24 10:38:47 -0300

    exercicio numero dois do livro python

--- convert_list_string.py
+++ convert_list_string.py
@@ -7,3 +7,35 @@ def list_string(listas):
 
 
 print(list_string(['banana', 'maça', 'coco']))
+
+
+# exercicio numero 2
+# Seu grid
+grid = [
+    ['.', '.', '.', '.', '.', '.'],
+    ['.', '0', '0', '.', '.', '.'],
+    ['0', '0', '0', '0', '.', '.'],
+    ['0', '0', '0', '0', '0', '.'],
+    ['.', '0', '0', '0', '0', '0'],
+    ['0', '0', '0', '0', '0', '.'],
+    ['0', '0', '0', '0', '.', '.'],
+    ['.', '0', '0', '.', '.', '.'],
+    ['.', '.', '.', '.', '.', '.'],
+]
+
+# tamanho da coluna e linha
+num_coluna = len(grid[0])
+num_linha = len(grid)
+
+linha_result = []
+
+for linha in range(num_linha):
+    str_result = ''
+
+    for coluna in range(num_coluna):
+        str_result += grid[linha][coluna]
+
+    linha_result.append(str_result)
+
+for linha in linha_result:
+    print(linha)
